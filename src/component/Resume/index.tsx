4001110b68b3c176f52ba00b3d016bc82ae2b23f
yun.park <yun.park@earlysloth.com>
2020-06-15 21:10:49 +0900

    add pdf viewer

--- src/component/Resume/index.tsx
+++ src/component/Resume/index.tsx
@@ -1,15 +1,16 @@
 import React, { useState, useContext, useEffect, useMemo } from "react";
 import "./styles.scss";
 
-import { Document, Page } from "react-pdf";
-
-// import resumeFile from "../../assets/resume.pdf";
+// import { Document, Page } from "react-pdf";
+let FileViewer: any = require("react-file-viewer");
+let resumeFile = require("../../assets/resume.pdf");
 export const Resume = () => {
   return (
     <div className="Resume">
-      {/* <FileViewer fileType="pdf" filePath={resumeFile} />
-       */}
-      {/* <Document file={resumeFile}>
+      <div className="innerPdf">
+        <FileViewer fileType="pdf" filePath={resumeFile} />
+      </div>
+      {/* <Document file={resume  File}>
         <Page pageNumber={1} />
       </Document> */}
     </div>
